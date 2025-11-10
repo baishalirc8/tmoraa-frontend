e685427aab72db88ce71675171bb19f1fa9d2df3
ankit-paul-keross <ankit.paul@keross.com>
2025-11-10 15:38:26 +0530

    minor

---
+++ src/components/PricePlanSection.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default function PricePlanSection() {
+  return (
+    <div className="mt-8 relative h-fit">
+      <div className="flex justify-center items-center">
+        <h2 className="text-4xl font-semibold">
+          Smarter Plans for Every F&B Business
+        </h2>
+      </div>
+      <p className="mt-5 px-56 text-center">
+        Choose a flexible plan that grows with you. Whether you operate one F&B
+        location or fifty, Tmoraa offers scalable pricing to match your business
+        size and goals
+      </p>
+    </div>
+  );
+}
